b0ad1411b393256f967a14923a92a6afe6f6866a
murata0531 <56902467+murata0531@users.noreply.github.com>
2020-06-24 04:26:04 +0900

    Add files via upload
    a

---
+++ Laravel+ReactでSPAを構築するサンプル/resources/js/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavBar from './NavBar'
+import About from './About'
+import User from './User'
+import UserDetail from './UserDetail'
+
+import Main from './Main'
+
+function App() {
+    return (
+    
+       
+    <Router>
+            <div>
+            <header>
+                <h1>SPA(シングルページアプリケーション)</h1>
+            </header>
+            <NavBar />
+            <Switch>
+                <Route path="/" exact component={Main} />
+                <Route path="/about" component={About} />
+                <Route path="/user" exact component={User} />
+                <Route path="/user/:id" component={UserDetail} />
+            </Switch>
+        </div>
+    </Router>
+    )
+}
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
---
+++ Laravel+ReactでSPAを構築するサンプル/resources/js/components/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+    return <h1>Topページ</h1>;
+}
+
+export default Main;
