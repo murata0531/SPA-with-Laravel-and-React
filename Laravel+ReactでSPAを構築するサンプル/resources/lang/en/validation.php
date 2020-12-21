399507c511888961fd400c80e1f08c1233c2182d
murata0531 <56902467+murata0531@users.noreply.github.com>
2020-12-22 07:17:08 +0900

    Update validation.php
    Update

--- Laravel+ReactでSPAを構築するサンプル/resources/lang/en/validation.php
+++ Laravel+ReactでSPAを構築するサンプル/resources/lang/en/validation.php
@@ -122,7 +122,7 @@ return [
     |--------------------------------------------------------------------------
     | Custom Validation Language Lines
     |--------------------------------------------------------------------------
-    |
+    |h
     | Here you may specify custom validation messages for attributes using the
     | convention "attribute.rule" to name the lines. This makes it quick to
     | specify a specific custom language line for a given attribute rule.
